7d3d157841610b35c700a25ba3868257fa57c651
Султан <carameleclipsimus@gmail.com>
2023-04-25 22:11:09 +0600

    feat: fourth task of methods is done

--- script.js
+++ script.js
@@ -37,3 +37,48 @@ const strArr = ["yes", "hello", "no", "easycode", "what"];
 
 const strArrEvery = strArr.every((element) => element.length > 3);
 // console.log(strArrEvery);
+
+// fourth
+/*
+Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
+
+[{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
+
+{char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
+
+{char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}]
+
+Напишите функцию, которая из элементов массива соберет и вернёт
+
+строку, основываясь на index каждой буквы. Например:
+
+[{char:"H",index:0}, {char:"i",index: 1}, {char:"!",index:2}] → “Hi!”
+*/
+const objArr = [
+  { char: "a", index: 12 },
+  { char: "w", index: 8 },
+  { char: "Y", index: 10 },
+  { char: "p", index: 3 },
+  { char: "p", index: 2 },
+
+  { char: "N", index: 6 },
+  { char: " ", index: 5 },
+  { char: "y", index: 4 },
+  { char: "r", index: 13 },
+  { char: "H", index: 0 },
+
+  { char: "e", index: 11 },
+  { char: "a", index: 1 },
+  { char: " ", index: 9 },
+  { char: "!", index: 14 },
+  { char: "e", index: 7 },
+];
+
+const objArrSort = objArr.sort((prev, next) => prev.index - next.index);
+
+const objArrSortReduce = objArrSort.reduce(
+  (acc, user) => (acc += user.char),
+  []
+);
+
+console.log(objArrSortReduce);

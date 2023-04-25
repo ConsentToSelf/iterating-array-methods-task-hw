3d9054ec5360695170d3323017ffec76aba5fc45
Султан <carameleclipsimus@gmail.com>
2023-04-25 22:13:52 +0600

    feat: five task of methods is done

--- script.js
+++ script.js
@@ -81,4 +81,16 @@ const objArrSortReduce = objArrSort.reduce(
   []
 );
 
-console.log(objArrSortReduce);
+// console.log(objArrSortReduce);
+
+// five
+
+/*
+Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы
+(размер массива определяется его длиной):
+[ [14, 45], [1], ['a', 'c', 'd'] ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+*/
+
+let sortArr = [[14, 45], [1], ["a", "c", "d"]];
+let resSortArr = sortArr.sort((prev, next) => prev.length - next.length);
+console.log(resSortArr);
